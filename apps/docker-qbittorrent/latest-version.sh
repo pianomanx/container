07acc3380d62d1e91a231057bee26d1df666a641
doob187 <60312740+doob187@users.noreply.github.com>
2021-11-29 00:38:36 +0100

    D3v (#1)
    * Create qBittorrent.conf
    
    * Create 30-config
    
    * Create run
    
    * Update Dockerfile
    
    * Update latest-version.sh
    
    * Update VERSION
    
    * Create PLATFORM

--- apps/docker-qbittorrent/latest-version.sh
+++ apps/docker-qbittorrent/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET http://ppa.launchpad.net/qbittorrent-team/qbittorrent-stable/ubuntu/dists/focal/main/binary-amd64/Packages.gz | gunzip -c|grep -A 7 -m 1 "Package: qbittorrent-nox" | awk -F ": " '/Version/{print $2;exit}')
+version=$(curl -sX GET "https://api.github.com/repos/qbittorrent/qBittorrent/tags" | jq --raw-output 'map(select(.name | test("^release-[0-9]+.[0-9]+.[0-9]+$"))) | first.name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
