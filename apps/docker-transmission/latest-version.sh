50ae1846951deb0fc7117ae9599de92aa029a611
doob187 <60312740+doob187@users.noreply.github.com>
2021-11-28 13:31:08 +0100

    Update latest-version.sh

--- apps/docker-transmission/latest-version.sh
+++ apps/docker-transmission/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/transmission/transmission/tags" | jq --raw-output '.[0].name')
+version=$(curl -sL "http://dl-cdn.alpinelinux.org/alpine/v3.14/community/x86_64/APKINDEX.tar.gz" | tar -xz -C /tmp && awk '/^P:transmission-daemon$/,/V:/' /tmp/APKINDEX | sed -n 2p | sed 's/^V://')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
