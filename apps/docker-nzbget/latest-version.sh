96c436e8569f991ce03ad822e84eb80165c3572c
doob187 <60312740+doob187@users.noreply.github.com>
2021-11-28 12:24:00 +0100

    Update latest-version.sh

--- apps/docker-nzbget/latest-version.sh
+++ apps/docker-nzbget/latest-version.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/nzbget/nzbget/releases/latest" | jq --raw-output '.tag_name')
+version=$(curl -sX GET "https://api.github.com/repos/nzbget/nzbget/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
