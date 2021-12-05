9e007c9b5c2432058801d1bdeb9f07c1c010ce57
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-05 21:11:33 +0100

    Update latest-version.sh

--- apps/docker-deluge/latest-version.sh
+++ apps/docker-deluge/latest-version.sh
@@ -2,7 +2,7 @@
 
 version="$(curl -sX GET http://ppa.launchpad.net/deluge-team/stable/ubuntu/dists/focal/main/binary-amd64/Packages.gz | \
    gunzip -c |grep -A 7 -m 1 '^Package: deluged$' | \
-   awk -F ': ' '/Version/{print $2;exit}')"
+   awk -F ': ' '/Version/{print $2;exit}' | sed -r 's/.{27}$//')"
 
 version="${version#*v}"
 version="${version#*release-}"
