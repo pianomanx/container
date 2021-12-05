ed2b4d95464d70c2e57d3e434a2afb6c904b61e1
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-05 20:56:31 +0100

    Major Update (#9)
    * Update Dockerfile
    
    * Update latest-version.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update latest-version.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Create latest-version.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update latest-version.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update latest-version.sh
    
    * Update latest-version.sh
    
    * Update Dockerfile
    
    * Create Dockerfile
    
    * Update Dockerfile
    
    * Update 20-config
    
    * Update Dockerfile
    
    * Update Dockerfile

--- apps/docker-mount/latest-version.sh
+++ apps/docker-mount/latest-version.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-version=$(curl -sX GET https://api.github.com/repos/linuxserver/docker-baseimage-alpine/releases/latest | jq --raw-output '. | .tag_name')
+version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/alpine/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
 printf "%s" "${version}"
