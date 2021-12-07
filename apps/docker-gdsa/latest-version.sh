6dd62aa016ddf3749daae958d0dd67063535e206
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-07 20:26:48 +0100

    Update latest-version.sh

--- apps/docker-gdsa/latest-version.sh
+++ apps/docker-gdsa/latest-version.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/google/cloud-sdk/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n4 | head -n1)
+version="$(curl -s "https://registry.hub.docker.com/v1/repositories/google/cloud-sdk/tags" \
+     | jq --raw-output '.[] | select(.name | contains(".")) | .name' \
+     | sort -t "." -k1,1n -k2,2n -k3,3n \
+     | sed -r 's/[-].*$//' | tail -n 1)"
 printf "%s" "${version}"
