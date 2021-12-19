793d34a920a65dd34cbdefca8651f0d2f27d1a0c
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-20 00:55:51 +0100

    Update mount.sh

--- apps/docker-mount/root/app/mount/mount.sh
+++ apps/docker-mount/root/app/mount/mount.sh
@@ -52,7 +52,7 @@ rclone mount remote: /mnt/remotes \
 --vfs-cache-max-age=${VFS_CACHE_MAX_AGE} \
 --vfs-cache-max-size=${VFS_CACHE_MAX_SIZE} \
 --vfs-read-chunk-size-limit=${VFS_READ_CHUNK_SIZE_LIMIT} \
---vfs-read-chunk-size=${VFS_READ_CHUNK_SIZE}
+--vfs-read-chunk-size=${VFS_READ_CHUNK_SIZE} \
 --rc --rc-user=${RC_USER} --rc-pass=${RC_PASSWORD} \
 --rc-addr=localhost:${RC_ADDRESS}
 
