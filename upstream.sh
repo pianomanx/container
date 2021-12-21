67a24748a27b879112cbf974ffa947bc30b28bd9
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-21 13:24:02 +0100

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -4,6 +4,7 @@ cd /media/container
 if [[ -n $(git status --porcelain) ]]; then
    git config --global user.name 'github-actions[bot]' 
    git config --global user.email 'github-actions[bot]@users.noreply.github.com' 
+   git repack -a -d --depth=500 --window=500
    git add -A
    git commit -sam "[UPSTREAM] Adding new features" || exit 0
    git push
