c549db5db005f59435d4deee2115b043e5f46735
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-26 13:25:48 +0100

    Update upload.sh

--- apps/docker-uploader/root/app/uploader/upload.sh
+++ apps/docker-uploader/root/app/uploader/upload.sh
@@ -53,10 +53,10 @@ chown -cR 1000:1000 "${LOGFILE}" "${VFS}" "${JSONFILERUN}"  1>/dev/null 2>&1
 rclone sync --tpslimit 32 --checkers=${CHECKERS} \
    --config=${rjson} --log-file=${LOGFILE} --log-level=${LOG_LEVEL} --stats 1s \
    --drive-chunk-size=32M --user-agent=${USERAGENT} ${BWLIMIT} \
-   "${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}"
+   "${downloadpath}/${FILEDIR}/${FILEBASE}/${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}/${FILE}"
 
-rclone check "${downloadpath}/${FILEDIR}/${FILEBASE}/${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}/${FILE}" | \
-rm -rf ${downloadpath}/${FILEDIR}/${FILEBASE}/${FILE}
+rclone check "${downloadpath}/${FILEDIR}/${FILEBASE}/${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}/${FILE}" \
+##rm -rf ${downloadpath}/${FILEDIR}/${FILEBASE}/${FILE}
 
 ENDTIME=$(date +%s)
 
