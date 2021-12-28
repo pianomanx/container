53be765c7b9f0391b935161ef702950bbddbbfc9
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-28 11:49:36 +0100

    Update docker-entrypoint.sh

--- apps/docker-rsync-backup/docker-entrypoint.sh
+++ apps/docker-rsync-backup/docker-entrypoint.sh
@@ -23,17 +23,6 @@ fi
 IFS=';'
 for exclude in ${EXCLUDES}; do
   echo "${exclude}" >>/backup_excludes
+
 done
-
-# Set the timezone. Base image does not contain the setup-timezone script, so an alternate way is used.
-if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
-  cp /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime && \
-  echo "${CONTAINER_TIMEZONE}" >/etc/timezone && \
-  echo "Container timezone set to: $CONTAINER_TIMEZONE"
-else
-  echo "Container timezone not modified"
-fi
-
-ntpd -nv
-
 exec "$@"
