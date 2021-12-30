7a9638cc6d1da403eb9bbb1325763ffb2bb03d77
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-30 14:11:26 +0100

    Update uploader.sh

--- apps/docker-uploader/root/app/uploader/uploader.sh
+++ apps/docker-uploader/root/app/uploader/uploader.sh
@@ -94,7 +94,7 @@ while true;do
       BWLIMIT="--bwlimit=${BANDWITHLIMIT}"
    fi
    pathglobal=/mnt/downloads
-   local="down:${pathglobal}"
+   SRC="local:${pathglobal}"
    DRIVEPERCENT=$(df --output=pcent ${pathglobal} | tr -dc '0-9')
    if [[ ! -z "${DRIVEUSEDSPACE}" ]]; then
       while true; do
@@ -107,7 +107,7 @@ while true;do
    fi
    log "STARTING DIFFMOVE FROM LOCAL TO REMOTE"
    rm -f ${CHK} ${DIFF} ${START}/${LOGFILE}
-   rclone check ${local} ${KEY}$[used]${CRYPTED}: --min-age=${MIN_AGE_UPLOAD}m \
+   rclone check ${SRC} ${KEY}$[used]${CRYPTED}: --min-age=${MIN_AGE_UPLOAD}m \
      --size-only --one-way --fast-list --exclude-from=${EXCLUDE} > ${CHK} 2>&1
    awk 'BEGIN { FS = ": " } /ERROR/ {print $2}' ${CHK} > ${DIFF}
    num_files=`cat ${CHK} | wc -l`
@@ -117,9 +117,9 @@ while true;do
       while IFS=$'\n' read -r -a modu; do
           chown -cR 1000:1000 ${pathglobal}/${modu[0]} > /dev/null
       done
-   log "STARTING RCLONE MOVE from ${local} to ${KEY}$[used]${CRYPTED}:"
+   log "STARTING RCLONE MOVE from ${SRC} to ${KEY}$[used]${CRYPTED}:"
    touch ${START}/${LOGFILE} 2>&1
-   rclone move --files-from ${CHK} ${local} ${KEY}$[used]${CRYPTED}: --stats=10s \
+   rclone move --files-from ${CHK} ${SRC} ${KEY}$[used]${CRYPTED}: --stats=10s \
      --drive-use-trash=false --drive-server-side-across-configs=true \
      --transfers ${TRANSFERS} --checkers=16 --use-mmap --cutoff-mode=soft \
      --use-json-log --log-file=${START}/${LOGFILE} --log-level=INFO \
@@ -127,7 +127,7 @@ while true;do
      --max-backlog=20000000 --tpslimit 32 --tpslimit-burst 32
    mv ${START}/${LOGFILE} ${DONE}/${LOGFILE} 
    rm -f ${CHK} ${DIFF}; }
-   log "DIFFMOVE FINISHED moving differential files from ${local} to GDSA$[used]${CRYPTED}:"
+   log "DIFFMOVE FINISHED moving differential files from ${SRC} to ${KEY}$[used]${CRYPTED}:"
    used=$(("${used}" + 1))
    echo "${used}" | tee "/system/uploader/.keys/lasteservicekey" > /dev/null
 
