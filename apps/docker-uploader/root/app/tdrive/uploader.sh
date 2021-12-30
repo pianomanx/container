fa6292bd4767654ae45a4ccad0dec38213b93fa5
doob187 <60312740+doob187@users.noreply.github.com>
2021-12-30 10:19:27 +0100

    Update uploader.sh

--- apps/docker-uploader/root/app/tdrive/uploader.sh
+++ apps/docker-uploader/root/app/tdrive/uploader.sh
@@ -14,142 +14,110 @@ function log() {
     echo "${1}"
 }
 
-path=/system/servicekeys/keys/
-downloadpath=/mnt/downloads
-#appbackups=/mnt/downloads/appbackups
-MOVE_BASE=${MOVE_BASE:-/}
-ENCRYPTED=${ENCRYPTED:-false}
-if [[ "${ENCRYPTED}" == "false" ]]; then
-    if grep -q GDSA1C /system/servicekeys/rclonegdsa.conf && grep -q GDSA2C /system/servicekeys/rclonegdsa.conf; then
-        ENCRYPTED=true
-    fi
-fi
-if [[ "${ENCRYPTED}" == "false" ]]; then
-   DRIVE=TDrive
-else
-   DRIVE=TCrypt
-fi
-
 log "dockserver.io Uploader started"
-rm -rf /app/uploader/pid/* \
-       /system/uploader/vfsforget/* \
-       /system/uploader/logs/* \
+rm -rf /app/uploader/pid/ \
+       /system/uploader/vfsforget/ \
+       /system/uploader/logs/ \
        /app/uploader/json/
 
 find ${downloadpath} -type f -name '*.lck' -delete
 
-GDSAARRAY=($(ls -1v ${path} | egrep '(PG|GD|GS)'))
-# shellcheck disable=SC2003
-GDSACOUNT=$(expr ${#GDSAARRAY[@]} - 1)
-# shellcheck disable=SC2086
-
-if [[ ${GDSACOUNT} -lt 1 ]]; then log "No accounts found to upload with, Exit" && exit 1; fi
-if [[ -d "/system/uploader/.stored/" ]]; then rm -rf /system/uploader/.stored/ >/dev/null; fi
-if [[ -d "/app/uploader" ]]; then chown -cR abc:abc /system/uploader/ 1>/dev/null 2>&1; fi
-
-if [[ -d ${downloadpath} ]]; then
-   mkdir -p ${downloadpath}/{torrent,nzb} &&  chown -cR 1000:1000 ${downloadpath} 1>/dev/null 2>&1
+rjson=/system/servicekeys/rclonegdsa.conf
+if `rclone config show --config=${rjson} | grep ":/encrypt" &>/dev/null`;then CRYPTED=C;fi
+if ! `rclone config show --config=${rjson} | grep "local" &>/dev/null`;then
+   rclone config create down local nunc 'true' --config=${rjson}
 fi
 
-if [[ -e "/system/uploader/.keys/lasteservicekey" ]]; then lastkeysused=$(cat /system/uploader/.keys/lasteservicekey); fi
-if [[ -e "/system/uploader/.keys/usedupload" ]];then usedupload=$(cat /system/uploader/.keys/usedupload); fi
-if [[ -e "/system/uploader/.keys/lastday" ]];then lastday=$(cat /system/uploader/.keys/lastday); fi
-if [[ ! -e "/system/uploader/.keys/lasteservicekey" ]]; then lastkeysused=0; fi
-if [[ ! -e "/system/uploader/.keys/usedupload" ]]; then usedupload=0; fi
-if [[ ! -e "/system/uploader/.keys/lastday" ]]; then
-   echo "0" >/system/uploader/.keys/lastday
+path=/system/servicekeys/keys/
+ARRAY=($(ls -1v ${path} | egrep '(PG|GD|GS)'))
+COUNT=$(expr ${#ARRAY[@]} - 1)
+
+if [[ ! -f "/system/uploader/.keys/lasteservicekey" ]]; then
+  used=0
+else
+  used=$(cat /system/uploader/.keys/lasteservicekey)
 fi
 
-BI="! -wholename '**.anchor' ! -wholename '*partial~*' ! -wholename '*_HIDDEN~' ! -wholename '*.fuse_hidden*' ! -wholename '*.lck' ! -wholename '*.version'"
-DI1="! -path '**.anchors/**' ! -path '**torrent/**' ! -path '**nzb/**' ! -path '**backup/**' ! -path '**nzbget/**' ! -path '**jdownloader2/**' ! -path '**sabnzbd/**'"
-DI2="! -path '**rutorrent/**' ! -path '**deluge/**' ! -path '**qbittorrent/**' ! -path '**amd/**' ! -path '**aria/**' ! -path '**tubesync/**' ! -path '**-vpn/**'"
-DI3="! -path '**_UNPACK_**' ! -path '**complete/**' ! -path '**torrents/**' ! -path '**temp/**' ! -path '.unionfs-fuse/*' ! -path '.unionfs/*' ! -path '**.inProgress/**'"
+if [[ ! -f /system/uploader/rclone.exclude ]]; then
+   cat > /system/uploader/rclone.exclude << EOF; $(echo)
+*-vpn/**
+torrent/**
+nzb/**
+nzbget/**
+.inProgress/**
+jdownloader2/**
+tubesync/**
+aria/**
+temp/**
+qbittorrent/**
+.anchors/**
+sabnzbd/**
+deluge/**
+EOF
+fi
 
-while true; do
-    source /system/uploader/uploader.env
-    if [[ ${DRIVEUSEDSPACE} != null ]]; then
-       pathglobal=/mnt/downloads
-       DRIVEPERCENT=$(df --output=pcent ${pathglobal} | tr -dc '0-9')
-       TARCHECK=$(find ${downloadpath} -type f -cmin +${MIN_AGE_UPLOAD} -name "**.tar.**" | wc -l)
-       while true; do
-          if [[ ${TARCHECK} != 0 ]]; then
-             sleep 1 && break
-          elif [[ ${DRIVEPERCENT} -ge ${DRIVEUSEDSPACE} ]]; then
-             sleep 1 && break
-          else
-             sleep 5 && continue
-          fi
-       done
-    fi
-    if [[ ${ADDITIONAL_IGNORES} == 'null' ]]; then ADDITIONAL_IGNORES=""; fi
-    mapfile -t files < <(eval find ${downloadpath} -cmin +${MIN_AGE_UPLOAD} -type f $BI $DI1 $DI2 $DI3 ${ADDITIONAL_IGNORES})
-    if [[ ${#files[@]} -gt 0 ]]; then
-        for i in "${files[@]}"; do
-            #mode chang for bypass double matched
-            chmod 755 "${i}"
-            FILEDIR=$(dirname "${i}" | sed "s#${downloadpath}${MOVE_BASE}##g")
-            if [[ -e "${i}.lck" ]]; then
-               log "Lock File found for ${i}" && continue
-            else
-                if [[ -e "${i}" ]]; then
-                    FILESIZE1=$(stat -c %s "${i}")
-                    sleep 1
-                    FILESIZE2=$(stat -c %s "${i}")
-                    if [[ "$FILESIZE1" -ne "$FILESIZE2" ]]; then
-                       log "Lock File found for ${i}" && continue
-                    fi
-                    # shellcheck disable=SC2010
-                    TRANSFERS=${TRANSFERS}
-                    ACTIVETRANSFERS=$(ls -l /app/uploader/pid/ | egrep -c  '*.trans')
-                    # shellcheck disable=SC2086
-                    if [[ ! ${ACTIVETRANSFERS} -ge ${TRANSFERS} ]]; then
-                        if [[ -e "${i}" ]]; then
-                            last=$(echo "${lastday} + ${FILESIZE2}" | bc)
-                            usedupload=$(echo "${usedupload} + ${FILESIZE2}" | bc)
-                            if [ ${ENCRYPTED} == "true" ]; then
-                                GDSA_TO_USE="${GDSAARRAY[$lastkeysused]}C"
-                            else
-                                GDSA_TO_USE="${GDSAARRAY[$lastkeysused]}"
-                            fi
-                            # Run upload script demonised
-                            /app/uploader/upload.sh "${i}" "${GDSA_TO_USE}" &
-                            PID=$!
-                            FILEBASE=$(basename "${i}")
-                            echo "${PID}" >"/app/uploader/pid/${FILEBASE}.trans"
-                            if [[ ${usedupload} -gt "763831531520" ]]; then
-                                log "${GDSAARRAY[$lastkeysused]} has hit 730GB switching to next SA"
-                                if [ "${lastkeysused}" -eq "${GDSACOUNT}" ]; then
-                                    lastkeysused=0
-                                    usedupload=0
-                                else
-                                    lastkeysused=$(("${lastkeysused}" + 1))
-                                    usedupload=0
-                                fi
-                                echo "${lastkeysused}" >/system/uploader/.keys/lasteservicekey
-                            fi
-                            log "${GDSAARRAY[${lastkeysused}]} is now $(echo "${usedupload}/1024/1024/1024" | bc -l) GiB"
-                            echo "${last}" >/system/uploader/.keys/lastday
-                            if [[ $(date +%H:%M) == "00:01" ]]; then
-                                echo "0" >/system/uploader/.keys/lastday
-                            else
-                                log "Uploaded to ${DRIVE} = $(echo "${lastday}/1024/1024/1024" | bc -l) GiB since reset"
-                             fi
-                             echo "${usedupload}" >/system/uploader/.keys/usedupload
-                        else
-                            log "File ${i} seems to have dissapeared"
-                        fi
-                    else
-                        log "Already ${ACTIVETRANSFERS} transfers running, waiting for next loop" && break
-                    fi
-                else
-                    log "File not found: ${i}" && continue
-                fi
-            fi
-        done
-        log "Finished looking for files, sleeping 5 secs"
-    else
-        log "Nothing to upload or file age is not reached,sleeping 5 secs"
-    fi
-    sleep 5
+LOGFILE=/tmp/rclone.json
+DIFF=/tmp/difflist.txt
+CHK=/tmp/check.log
+EXCLUDE=/system/uploader/rclone.exclude
+
+while true;do 
+   re='^[0-9]+$'
+   if [ "${used}" -eq "${COUNT}" ]; then
+      used=1
+   else
+      used=${used}
+   fi
+
+   source /system/uploader/uploader.env
+   TRANSFERS=${TRANSFERS}
+   DRIVEUSEDSPACE=${DRIVEUSEDSPACE}
+   BANDWITHLIMIT=${BANDWITHLIMIT}
+
+   if ! [ ${BANDWITHLIMIT} =~ $re ]; then
+      BWLIMIT=""
+   else
+      BANDWITHLIMIT=${BANDWITHLIMIT}
+      BWLIMIT="--bwlimit=${BANDWITHLIMIT}"
+   fi
+
+   pathglobal=/mnt/downloads
+   local="down:${pathglobal}"
+
+   DRIVEPERCENT=$(df --output=pcent ${pathglobal} | tr -dc '0-9')
+   if [[ ${DRIVEUSEDSPACE} =~ $re ]]; then
+      while true; do
+        if [[ ${DRIVEPERCENT} -ge ${DRIVEUSEDSPACE} ]]; then
+           sleep 1 && break
+        else
+           sleep 5 && continue
+        fi
+      done
+   fi
+   log "STARTING DIFFMOVE FROM LOCAL TO REMOTE"
+   rm -f ${CHK} ${DIFF} ${LOGFILE}
+   set -x
+   rclone check ${local} GDSA$[used]${CRYPTED}: --min-age=10m \
+     --size-only --one-way --fast-list \
+     --exclude-from=${EXCLUDE} > ${CHK} 2>&1
+   set +x
+   awk 'BEGIN { FS = ": " } /ERROR/ {print $2}' check.log > ${DIFF}
+   num_files=`cat ${CHK} | wc -l`
+   log "Number of files to be moved $num_files"
+   [ $num_files -gt 0 ] && {
+   log "STARTING RCLONE MOVE from ${local} to GDSA$[used]${CRYPTED}:"
+   touch ${LOGFILE} 2>&1
+   rclone move --files-from ${CHK} ${local} GDSA$[used]${CRYPTED}: --stats=10s \
+     --drive-use-trash=false --drive-server-side-across-configs=true \
+     --transfers ${TRANSFERS} --checkers=16 --use-mmap --cutoff-mode=soft \
+     --use-json-log --log-file=${LOGFILE} --log-level=INFO \
+     --user-agent=${USERAGENT} ${BWLIMIT} --config=${rjson}  \
+     --max-backlog=2000 --tpslimit 32 --tpslimit-burst 32
+   rm -f ${CHK} ${DIFF} ${LOGFILE} ; }
+   log "DIFFMOVE FINISHED moving differential files from ${local} to GDSA$[used]${CRYPTED}:"
+   used=$(("${used}" + 1))
+   echo ${used} > /opt/appdata/system/uploader/.keys/lasteservicekey
+
+sleep 60
+
 done
-#E-o-F#
