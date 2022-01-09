e85f9ef0953822d0d4e9502ed4fa0f92b16a1d9e
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-09 18:26:54 +0100

    Update mount.sh

--- apps/docker-mount/root/app/mount/mount.sh
+++ apps/docker-mount/root/app/mount/mount.sh
@@ -21,71 +21,6 @@ source /app/mount/function.sh
 
 mkdir -p ${TMPRCLONE} ${REMOTE}
 
-function rcx() {
-
-rclone rc mount/mount \
-   --rc-user=${RC_USER} --rc-pass=${RC_PASSWORD} \
-   --config=${CONFIG} --cache-dir=${TMPRCLONE} \
-   fs=remote: mountPoint=${REMOTE} mountType=mount \
-   logOpt='{
-   "File": "${MLOG}",
-   "Format": "date,time",
-   "LogSystemdSupport": false
-   }'
-   mainOpt='{
-   "BufferSize": ${BUFFER_SIZE},
-   "Checkers": 32,
-   "TPSLimit": $[TPSLIMIT},
-   "TPSLimitBurst": ${TPSBURST},
-   "UseListR": true,
-   "UseMmap": true,
-   "UseServerModTime": true,
-   "TrackRenames": true,
-   "UserAgent": "${UAGENT}"
-   }'
-   vfsOpt='{
-   "CacheMaxAge": ${VFS_CACHE_MAX_AGE},
-   "CacheMaxSize": ${VFS_CACHE_MAX_SIZE},
-   "CacheMode": 3,
-   "CachePollInterval": ${VFS_CACHE_POLL_INTERVAL},
-   "CaseInsensitive": false,
-   "ChunkSize": ${VFS_READ_CHUNK_SIZE},
-   "ChunkSizeLimit": ${VFS_READ_CHUNK_SIZE_LIMIT},
-   "DirCacheTime": ${DIR_CACHE_TIME},
-   "GID": ${PGID},
-   "NoChecksum": false,
-   "NoModTime": true,
-   "NoSeek": true,
-   "PollInterval": ${POLL_INTERVAL},
-   "UID": ${PUID},
-   "Umask": ${UMASK}
-   }' 
-   mountOpt='{
-   "AllowNonEmpty": true,
-   "AllowOther": true,
-   "AsyncRead": true,
-   "Daemon": true,
-   "AllowOther": true
-   }'
-
-   rclone rc vfs/refresh recursive=true --fast-list \
-   --rc-user=${RC_USER} \
-   --rc-pass=${RC_PASSWORD} \
-   --rc-addr=localhost:${RC_ADDRESS} \
-   --config=${CONFIG} \
-   --log-file=${RLLOG} \
-   --log-level=${LOGLEVEL_RC}
-}
-
-function rckill() {
-   rclone rc mount/unmount \
-   mountPoint=${REMOTE} \
-   --config=${CONFIG} \
-   --rc-user=${RC_USER} \
-   --rc-pass=${RC_PASSWORD} \
-   --rc-addr=${RC_ADDRESS}
-}
-
 rclone rcd --rc-user=${RC_USER} \
   --rc-pass=${RC_PASSWORD} \
   --rc-addr=localhost:${RC_ADDRESS} \
@@ -95,7 +30,7 @@ while true; do
    if [ "$(ls -A /mnt/unionfs)" ] && [ "$(ps aux | grep -i 'rclone rc mount/mount' | grep -v grep)" != "" ]; then
       sleep 360
    else
-      rckill && sleep 10 && rcx && sleep 360
+      rckill && sleep 5 && rcx && refreshVFS && sleep 360
    fi
 done
 
