8b224fbdf3e3007cfbf1c9c7e843c63b5b5da362
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-09 20:09:36 +0100

    Update function.sh

--- apps/docker-mount/root/app/mount/function.sh
+++ apps/docker-mount/root/app/mount/function.sh
@@ -16,29 +16,43 @@
 # shellcheck disable=SC2002
 # shellcheck disable=SC2006
 ## FUNCTIONS SOURCECONFIG ##
+#########################################
+# From here on out, you probably don't  #
+#   want to change anything unless you  #
+#   know what you're doing.             #
+#########################################
 
-### D0 NOT CHANGE ANY LINES ###
 source /system/mount/mount.env
 
+#SETTINGS
 CONFIG=/app/rclone/rclone.conf
+ENVA=/system/mount/mount.env
+TMPENV=/tmp/mount.env
+GDSAMIN=4
+ARRAY=$(ls -A ${JSONDIR} | wc -l )
+
+#SCRIPTS
+SROTATE=/app/mount/rotation.sh
+SCRIPT=/app/mount/mount.sh
+SDISCORD=/app/discord/discord.sh
+
+#FOLDER
 REMOTE=/mnt/unionfs
-MLOG=/system/mount/logs/rclone-union.log
-RLOG=/system/mount/logs/vfs-refresh.log
 JSONDIR=/system/mount/keys
-REMOTE=/mnt/unionfs
 SMOUNT=/app/mount
 FDISCORD=/app/discord
 LFOLDER=/app/language/mount
-SDISCORD=/app/discord/discord.sh
-LOG=/tmp/discord.dead
-MLOG=/system/mount/logs/rclone-union.log
-SMOUNT=/app/mount
-SROTATE=/app/mount/rotation.sh
-SCRIPT=/app/mount/mount.sh
-GDSAMIN=4
-ARRAY=$(ls -A ${JSONDIR} | egrep -c '*.json')
 
-### D0 NOT CHANGE ANY LINES ###
+#LOG
+MLOG=/system/mount/logs/rclone-union.log
+RLOG=/system/mount/logs/vfs-refresh.log
+DLOG=/tmp/discord.dead
+
+#########################################
+# From here on out, you probably don't  #
+#   want to change anything unless you  #
+#   know what you're doing.             #
+#########################################
 
 function log() {
 
@@ -54,9 +68,17 @@ function run() {
 
 function checkban() {
 
-  tail -n 1 "${MLOG}" | grep --line-buffered 'googleapi: Error' | while read; do
-     if [[ ! ${DISCORD_SEND} != "null" ]]; then discord ; else log "${startuphitlimit}" ; fi
-     if [[ ${ARRAY} != 0 ]]; then run "${SROTATE}" && log "${startuprotate}" ; fi
+   tail -n 1 "${MLOG}" | grep --line-buffered 'googleapi: Error' | while read; do
+       if [[ ! ${DISCORD_SEND} != "null" ]]; then
+          discord
+       else
+          log "${startuphitlimit}"
+       fi
+
+       if [[ ${ARRAY} != 0 ]]; then
+          run "${SROTATE}"
+          log "${startuprotate}" 
+       fi
    done
 
 }
@@ -65,22 +87,28 @@ function discord() {
 
    source /system/mount/mount.env
    DATE=$(date "+%Y-%m-%d")
+   YEAR=$(date "+%Y")
+
    if [[ ${ARRAY} -gt 0 ]]; then
       MSG1=${startuphitlimit}
       MSG2=${startuprotate}
       MSGSEND="${MSG1} and ${MSG2}"
-      rm -rf ${LOG}
+      rm -rf ${DLOG}
    else
       MSG1=${startuphitlimit}
       MSGSEND="${MSG1}"
    fi
-   YEAR=$(date "+%Y")
-   if [[ ! -d "${FDISCORD}" ]]; then mkdir -p "${FDISCORD}"; fi
-      if [[ ! -f "${SDISCORD}" ]]; then
-         curl --silent -fsSL https://raw.githubusercontent.com/ChaoticWeg/discord.sh/master/discord.sh -o "${SDISCORD}"
-         chmod 755 "${SDISCORD}"
-      fi
-   if [[ ! -f "${LOG}" ]]; then
+
+   if [[ ! -d "${FDISCORD}" ]]; then
+      mkdir -p "${FDISCORD}"
+   fi
+
+   if [[ ! -f "${SDISCORD}" ]]; then
+      curl --silent -fsSL https://raw.githubusercontent.com/ChaoticWeg/discord.sh/master/discord.sh -o "${SDISCORD}"
+      chmod 755 "${SDISCORD}"
+   fi
+
+   if [[ ! -f "${DLOG}" ]]; then
       run "${SDISCORD}" \
       --webhook-url=${DISCORD_WEBHOOK_URL} \
       --title "${DISCORD_EMBED_TITEL}" \
@@ -93,27 +121,24 @@ function discord() {
       --thumbnail "https://www.freeiconspng.com/uploads/error-icon-4.png" \
       --footer "(c) ${YEAR} DockServer.io" \
       --footer-icon "https://www.freeiconspng.com/uploads/error-icon-4.png" \
-      --timestamp >${LOG}
+      --timestamp > "${DLOG}"
    fi
 
 }
 
 function envrenew() {
 
-   file1=/system/mount/mount.env
-   file2=/tmp/mount.env
-   diff -q "$file1" "$file2"
+   diff -q "$ENVA" "$TMPENV"
    if [ $? -gt 0 ]; then
       rckill && run "${SCRIPT}"
     else
-      echo "no changes" > /tmp/dead.lock
+      echo "no changes" > "${NLOG}"
    fi
 
 }
 
 function lang() {
 
-   source /system/mount/mount.env
    LANGUAGE=${LANGUAGE}
    startupmount=$(grep -Po '"startup.mount": *\K"[^"]*"' "${LFOLDER}/${LANGUAGE}.json" | sed 's/"\|,//g')
    startuphitlimit=$(grep -Po '"startup.hitlimit": *\K"[^"]*"' "${LFOLDER}/${LANGUAGE}.json" | sed 's/"\|,//g')
@@ -121,12 +146,20 @@ function lang() {
    startupnewchanges=$(grep -Po '"startup.newchanges": *\K"[^"]*"' "${LFOLDER}/${LANGUAGE}.json" | sed 's/"\|,//g')
    startuprcloneworks=$(grep -Po '"startup.rcloneworks": *\K"[^"]*"' "${LFOLDER}/${LANGUAGE}.json" | sed 's/"\|,//g')
    currenttime=$(date +%H:%M)
+
    if [[ "$currenttime" > "23:59" ]] || [[ "$currenttime" < "00:01" ]]; then
       if [[ -d "/app/language" ]]; then
-         git -C "${LFOLDER}/" stash --quiet && git -C "${LFOLDER}/" pull --quiet && cd "${LFOLDER}/" && git stash clear
+         git -C "${LFOLDER}/" stash --quiet
+         git -C "${LFOLDER}/" pull --quiet
+         cd "${LFOLDER}/"
+         git stash clear
       fi
    fi
-   if [[ ! -d "/app/language" ]]; then mkdir -p "${LFOLDER}/" && git -C /app clone https://github.com/dockserver/language.git ; fi
+
+   if [[ ! -d "/app/language" ]]; then
+      mkdir -p "${LFOLDER}/"
+      git -C /app clone https://github.com/dockserver/language.git
+   fi
 
 }
 
@@ -139,87 +172,111 @@ function startup() {
 
 function rcx() {
 
-  rclone rc mount/mount \
-   --rc-user=${RC_USER} --rc-pass=${RC_PASSWORD} \
-   --config=${CONFIG} --cache-dir=${TMPRCLONE} \
-   fs=remote: mountPoint=${REMOTE} mountType=mount \
-   logOpt='{
-   "File": "${MLOG}",
-   "Format": "date,time",
-   "LogSystemdSupport": false
-   }'
-   mainOpt='{
-   "BufferSize": ${BUFFER_SIZE},
-   "Checkers": 32,
-   "TPSLimit": $[TPSLIMIT},
-   "TPSLimitBurst": ${TPSBURST},
-   "UseListR": true,
-   "UseMmap": true,
-   "UseServerModTime": true,
-   "TrackRenames": true,
-   "UserAgent": "${UAGENT}"
-   }'
-   vfsOpt='{
-   "CacheMaxAge": ${VFS_CACHE_MAX_AGE},
-   "CacheMaxSize": ${VFS_CACHE_MAX_SIZE},
-   "CacheMode": 3,
-   "CachePollInterval": ${VFS_CACHE_POLL_INTERVAL},
-   "CaseInsensitive": false,
-   "ChunkSize": ${VFS_READ_CHUNK_SIZE},
-   "ChunkSizeLimit": ${VFS_READ_CHUNK_SIZE_LIMIT},
-   "DirCacheTime": ${DIR_CACHE_TIME},
-   "GID": ${PGID},
-   "NoChecksum": false,
-   "NoModTime": true,
-   "NoSeek": true,
-   "PollInterval": ${POLL_INTERVAL},
-   "UID": ${PUID},
-   "Umask": ${UMASK}
-   }' 
-   mountOpt='{
-   "AllowNonEmpty": true,
-   "AllowOther": true,
-   "AsyncRead": true,
-   "Daemon": true,
-   "AllowOther": true
-   }'
+   rclone rc mount/mount \
+     --rc-user=${RC_USER} \
+     --rc-pass=${RC_PASSWORD} \
+     --config=${CONFIG} \
+     --cache-dir=${TMPRCLONE} \
+     fs=remote: \
+     mountPoint=${REMOTE} \
+     mountType=mount \
+     logOpt='{
+     "File": "${MLOG}",
+     "Format": "date,time",
+     "LogSystemdSupport": false
+     }'
+     mainOpt='{
+     "BufferSize": ${BUFFER_SIZE},
+     "Checkers": 32,
+     "TPSLimit": $[TPSLIMIT},
+     "TPSLimitBurst": ${TPSBURST},
+     "UseListR": true,
+     "UseMmap": true,
+     "UseServerModTime": true,
+     "TrackRenames": true,
+     "UserAgent": "${UAGENT}"
+     }'
+     vfsOpt='{
+     "CacheMaxAge": ${VFS_CACHE_MAX_AGE},
+     "CacheMaxSize": ${VFS_CACHE_MAX_SIZE},
+     "CacheMode": 3,
+     "CachePollInterval": ${VFS_CACHE_POLL_INTERVAL},
+     "CaseInsensitive": false,
+     "ChunkSize": ${VFS_READ_CHUNK_SIZE},
+     "ChunkSizeLimit": ${VFS_READ_CHUNK_SIZE_LIMIT},
+     "DirCacheTime": ${DIR_CACHE_TIME},
+     "GID": ${PGID},
+     "UID": ${PUID},
+     "NoChecksum": false,
+     "NoModTime": true,
+     "NoSeek": true,
+     "PollInterval": ${POLL_INTERVAL},
+     "Umask": ${UMASK}
+     }' 
+     mountOpt='{
+     "AllowNonEmpty": true,
+     "AllowOther": true,
+     "AsyncRead": true,
+     "Daemon": true,
+     "AllowOther": true
+     }'
 
 }
 
 function refreshVFS() {
 
-   rclone rc vfs/refresh recursive=true --fast-list \
-   --rc-user=${RC_USER} \
-   --rc-pass=${RC_PASSWORD} \
-   --rc-addr=localhost:${RC_ADDRESS} \
-   --config=${CONFIG} \
-   --log-file=${RLLOG} \
-   --log-level=${LOGLEVEL_RC}
+   rclone rc vfs/refresh \
+      recursive=true \
+      --fast-list \
+      --rc-user=${RC_USER} \
+      --rc-pass=${RC_PASSWORD} \
+      --rc-addr=localhost:${RC_ADDRESS} \
+      --config=${CONFIG} \
+      --log-file=${RLLOG} \
+      --log-level=${LOGLEVEL_RC}
 
 }
 
 function rckill() {
 
    rclone rc mount/unmount \
-   mountPoint=${REMOTE} \
-   --config=${CONFIG} \
-   --rc-user=${RC_USER} \
-   --rc-pass=${RC_PASSWORD} \
-   --rc-addr=${RC_ADDRESS}
+      mountPoint=${REMOTE} \
+      --config=${CONFIG} \
+      --rc-user=${RC_USER} \
+      --rc-pass=${RC_PASSWORD} \
+      --rc-addr=${RC_ADDRESS}
 
 }
 
 function drivecheck() {
 
-  if [ "$(ls -A /mnt/unionfs)" ] && [ "$(ps aux | grep -i 'rclone rc mount/mount' | grep -v grep)" != "" ]; then
-     rclone rc fscache/clear --fast-list --rc-user=${RC_USER} --rc-pass=${RC_PASSWORD} \
-        --rc-addr=localhost:${RC_ADDRESS} --config=${CONFIG} --log-file=${RLOG} --log-level=${LOGLEVEL_RC}
-     sleep 5
-     rclone rc vfs/refresh recursive=true --fast-list \
-        --rc-user=${RC_USER} --rc-pass=${RC_PASSWORD} --rc-addr=localhost:${RC_ADDRESS} \
-        --config=${CONFIG} --log-file=${RLOG} --log-level=${LOGLEVEL_RC}
-  fi
+   if [ "$(ls -A /mnt/unionfs)" ] && [ "$(ps aux | grep -i 'rclone rc mount/mount' | grep -v grep)" != "" ]; then
+      rclone rc fscache/clear \
+         --fast-list \
+         --rc-user=${RC_USER} \
+         --rc-pass=${RC_PASSWORD} \
+         --rc-addr=localhost:${RC_ADDRESS} \
+         --config=${CONFIG} \
+         --log-file=${RLOG} \
+         --log-level=${LOGLEVEL_RC}
+
+      rclone rc vfs/refresh \
+         recursive=true \
+         --fast-list \
+         --rc-user=${RC_USER} \
+         --rc-pass=${RC_PASSWORD} \
+         --rc-addr=localhost:${RC_ADDRESS} \
+         --config=${CONFIG} \
+         --log-file=${RLOG} \
+         --log-level=${LOGLEVEL_RC}
+   fi
 
 }
 
-### D0 NOT CHANGE ANY LINES ###
+#########################################
+# Till here on out, you probably don't  #
+#   want to change anything unless you  #
+#   know what you're doing.             #
+#########################################
+     ### DO NOT MAKE ANY CHANGES ###
+### IF YOU DON'T KNOW WHAT YOU ARE DOING ###
