05206ae487081762b20057fbc79266660ced63b9
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-13 19:49:44 +0100

    Update uploader.sh

--- apps/docker-uploader/root/app/uploader/uploader.sh
+++ apps/docker-uploader/root/app/uploader/uploader.sh
@@ -15,7 +15,9 @@ function log() {
     echo "${1}"
 }
 
-if pidof -o %PPID -x "$0"; then exit 1; fi
+if pidof -o %PPID -x "$(basename $0)"; then
+   exit 1
+fi
 
 log "dockserver.io Multi-Thread Uploader started"
 
