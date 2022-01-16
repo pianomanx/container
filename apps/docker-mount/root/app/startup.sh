23e88cd210bdfd8e2ee21f4a38cdf4c5f36ba137
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-16 22:58:40 +0100

    Update startup.sh

--- apps/docker-mount/root/app/startup.sh
+++ apps/docker-mount/root/app/startup.sh
@@ -18,18 +18,28 @@
 function log() {
    echo "[Mount] ${1}"
 }
-if pidof -o %PPID -x "$(basename $0)"; then
+if pidof -o %PPID -x "$0"; then
    exit 1
 fi
+
+## Bad rclone
+cp -r /app/rclone/rclone.conf /root/.config/rclone/
+## Bad rclone
 
 source /system/mount/mount.env
 source /app/mount/function.sh
 
-lang
+lang && sleep 5 || exit 1
 LANGUAGE=${LANGUAGE}
 startupmount=$(grep -Po '"startup.mount": *\K"[^"]*"' "${LFOLDER}/${LANGUAGE}.json" | sed 's/"\|,//g')
 log "${startupmount}"
-bash "${SCRIPT}"
+
+mkdir -p "${TMPRCLONE}" "${REMOTE}" && sleep 5 || exit 1
+
+rcstart && sleep 5 || exit 1
+rcset && sleep 5 || exit 1
+rcmount && sleep 5 || exit 1
+
 sleep 120
 
 while true; do
