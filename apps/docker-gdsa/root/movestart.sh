49e9bfdcf7a7416a6ff2ce37d2e2e9b250ac4d20
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-20 19:16:35 +0100

    Update movestart.sh

--- apps/docker-gdsa/root/movestart.sh
+++ apps/docker-gdsa/root/movestart.sh
@@ -19,8 +19,8 @@
 # shellcheck disable=SC2046
 
 cat > /etc/apk/repositories << EOF; $(echo)
-http://dl-cdn.alpinelinux.org/alpine/v3.15)/main
-http://dl-cdn.alpinelinux.org/alpine/v3.15/community
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/main
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
