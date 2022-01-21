81802dd21251c144ff1dd6534440ab3a3d2c15a5
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-21 20:56:49 +0100

    Update latest-version.sh

--- apps/docker-wireguard/latest-version.sh
+++ apps/docker-wireguard/latest-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -X GET "https://api.github.com/repos/WireGuard/wireguard-tools/tags" | jq --raw-output '.[0].name')"
+version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/WireGuard/wireguard-tools/tags" | jq --raw-output '.[0].name')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
