cd33c2ee10c51c83085ac597168d789a2ebd6fb8
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-21 20:55:01 +0100

    Update latest-version.sh

--- apps/docker-requestrr/latest-version.sh
+++ apps/docker-requestrr/latest-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -X GET "https://api.github.com/repos/darkalfx/requestrr/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')"
+version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/darkalfx/requestrr/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')"
 version="${version#*v}"
 version="${version#*V}"
 version="${version#*release-}"
