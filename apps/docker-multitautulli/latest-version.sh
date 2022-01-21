d5642de3e58983ed3ac213033eed00fb518bc39c
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-21 20:53:16 +0100

    Update latest-version.sh

--- apps/docker-multitautulli/latest-version.sh
+++ apps/docker-multitautulli/latest-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -X GET "https://api.github.com/repos/zSeriesGuy/Tautulli/releases/latest" | jq --raw-output '. | .tag_name')"
+version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/zSeriesGuy/Tautulli/releases/latest" | jq --raw-output '. | .tag_name')"
 version="${version#*V}"
 version="${version#*v}"
 version="${version#*release-}"
