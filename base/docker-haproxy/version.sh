46cd411e5fcd024b052f53342f23f0b0b43aa75c
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-23 23:39:19 +0100

    Update version.sh

--- base/docker-haproxy/version.sh
+++ base/docker-haproxy/version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -s "https://registry.hub.docker.com/v1/repositories/library/haproxy/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | grep -vE '*dev*' | grep -vE '*bullsey*' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)"
-version="${version#*v}"
-version="${version#*release-}"
+version=$(curl -sX GET "https://registry.hub.docker.com/v1/repositories/library/haproxy/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | grep -vE '*dev*' | grep -vE '*bullsey*'  | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
+#version="${version#*v}"
+#version="${version#*release-}"
 printf "%s" "${version}"
