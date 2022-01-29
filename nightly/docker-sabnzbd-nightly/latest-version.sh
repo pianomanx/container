34473580f3cbb16ee435c3e0f0c0eb2ade271014
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-29 23:05:22 +0100

    Update latest-version.sh

--- nightly/docker-sabnzbd-nightly/latest-version.sh
+++ nightly/docker-sabnzbd-nightly/latest-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -sX GET https://api.github.com/repos/sabnzbd/sabnzbd/releases | jq -r 'first(.[]) | .tag_name')"
+version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/sabnzbd/sabnzbd/commits/develop" | jq -r .sha)"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
