8c0db115b0cc4c840f2d4d286b70637e3fea3499
doob187 <60312740+doob187@users.noreply.github.com>
2022-01-30 13:00:06 +0100

    Update latest-version.sh

--- apps/docker-qbittorrent/latest-version.sh
+++ apps/docker-qbittorrent/latest-version.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET http://ppa.launchpad.net/qbittorrent-team/qbittorrent-stable/ubuntu/dists/focal/main/binary-amd64/Packages.gz | \
-    gunzip -c |grep -A 7 -m 1 'Package: qbittorrent-nox' | awk -F ': ' '/Version/{print $2;exit}' | sed -e 's/1://g' | sed -r 's/.{42}$//')"
-
+version="$(curl -fsSL "http://dl-cdn.alpinelinux.org/alpine/edge/community/x86_64/APKINDEX.tar.gz" | tar -xz -C /tmp && \
+       awk '/^P:qbittorrent-nox$/,/V:/' /tmp/APKINDEX | sed -n 2p | sed 's/^V://')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
