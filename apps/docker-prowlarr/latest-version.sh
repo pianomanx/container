917c64314bb1be2a0c554cb332a39208108e316b
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-03 16:37:45 +0100

    Update latest-version.sh

--- apps/docker-prowlarr/latest-version.sh
+++ apps/docker-prowlarr/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/nightly/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version')
+PROWLARR_BRANCH="nightly"
+version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/${PROWLARR_BRANCH}/changes?runtime=netcore&os=linuxmusl" | jq -r '.[0].version')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
