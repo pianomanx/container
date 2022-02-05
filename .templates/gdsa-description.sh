3789e0ff9ee7fc61575aef77642d58c92bacd520
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:01:25 +0100

    Update gdsa-description.sh

--- .templates/gdsa-description.sh
+++ .templates/gdsa-description.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-version="$(curl -s "https://registry.hub.docker.com/v1/repositories/google/cloud-sdk/tags" \
-     | jq --raw-output '.[] | select(.name | contains(".")) | .name' \
-     | sort -t "." -k1,1n -k2,2n -k3,3n \
-     | sed -r 's/[-].*$//' | tail -n 1)"
-printf "%s" "${version}"
+## NO github Repo ##
+## set own DESCRIPTION ##
+DESCRIPTION="Docker container for JDownloader 2"
+printf "%s" "${DESCRIPTION}"
