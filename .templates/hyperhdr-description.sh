e894701ed9606b31eb7864c46841e87103fcbffb
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 16:45:51 +0100

    Update hyperhdr-description.sh

--- .templates/hyperhdr-description.sh
+++ .templates/hyperhdr-description.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+username=$1
+token=$2
 DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/awawa-dev/HyperHDR" | jq -r '.description')"
 printf "%s" "${DESCRIPTION}"
