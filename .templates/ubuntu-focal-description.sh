a5e33d99747b93429b0ad029e9d10a8857359711
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:22:09 +0100

    Update ubuntu-focal-description.sh

--- .templates/ubuntu-focal-description.sh
+++ .templates/ubuntu-focal-description.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/alpine/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
-
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+## NO github Repo ##
+## set own DESCRIPTION ##
+DESCRIPTION="Dockerbase Image with Ubuntu focal"
+printf "%s" "${DESCRIPTION}"
