db804368dbed990007eee9df2159e6fb93bf6214
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 17:26:49 +0100

    Update bazarr-description.sh

--- .templates/bazarr-description.sh
+++ .templates/bazarr-description.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 username=$1
 token=$2
-
 DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/morpheus65535/bazarr" | jq -r '.description')"
 printf "%s" "${DESCRIPTION}"
