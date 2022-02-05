086c204ba0235316de05ae2c5949fd7bafca8af7
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:10:19 +0100

    Update nzbget-description.sh

--- .templates/nzbget-description.sh
+++ .templates/nzbget-description.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
- 
-version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/nzbget/nzbget/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+username=$1
+token=$2
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/nzbget/nzbget" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
