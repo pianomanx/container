8e5c1ccced838ba8cbb2ebeffb230db728d80ee5
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:03:30 +0100

    Update jellyfin-description.sh

--- .templates/jellyfin-description.sh
+++ .templates/jellyfin-description.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET https://repo.jellyfin.org/ubuntu/dists/focal/main/binary-amd64/Packages | grep -A 8 -m 1 'Package: jellyfin-server' | awk -F ': ' '/Version/{print $2;exit}')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+username=$1
+token=$2
+DESCRIPTION="$(curl -sX GET "https://api.github.com/repos/jellyfin/jellyfin" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
