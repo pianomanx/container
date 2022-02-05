4caf6bc40430f782439f0a1d0c0725f7651a2ea6
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 17:28:20 +0100

    Update deluge-description.sh

--- .templates/deluge-description.sh
+++ .templates/deluge-description.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-
-version="$(curl -sX GET http://ppa.launchpad.net/deluge-team/stable/ubuntu/dists/focal/main/binary-amd64/Packages.gz | \
-   gunzip -c |grep -A 7 -m 1 '^Package: deluged$' | \
-   awk -F ': ' '/Version/{print $2;exit}' | sed -r 's/.{27}$//')"
-
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+username=$1
+token=$2
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/deluge-torrent/deluge" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
