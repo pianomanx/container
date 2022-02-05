c6bc6ca078a96a6eda5ff37b86a35af13bcaf3b2
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 16:44:24 +0100

    Update haproxy-description.sh

--- .templates/haproxy-description.sh
+++ .templates/haproxy-description.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-
+username=$1
+token=$2
 DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/haproxy/haproxy" | jq -r '.description')"
 printf "%s" "${DESCRIPTION}"
