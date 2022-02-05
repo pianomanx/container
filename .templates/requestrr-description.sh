3eb10e585a1bbedf180412c7dd7f8bf54a03d775
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 17:34:56 +0100

    Update requestrr-description.sh

--- .templates/requestrr-description.sh
+++ .templates/requestrr-description.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/darkalfx/requestrr/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')"
-version="${version#*v}"
-version="${version#*V}"
-version="${version#*release-}"
-printf "%s" "${version}"
+username=$1
+token=$2
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/darkalfx/requestrr" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
