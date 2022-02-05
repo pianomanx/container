ef8c7e48b48c8dda052e04dbd3561d6f8809e70d
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:30:02 +0100

    Update qbittorrent-description.sh

--- .templates/qbittorrent-description.sh
+++ .templates/qbittorrent-description.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-version="$(curl -fsSL "http://dl-cdn.alpinelinux.org/alpine/edge/community/x86_64/APKINDEX.tar.gz" | tar -xz -C /tmp && \
-       awk '/^P:qbittorrent-nox$/,/V:/' /tmp/APKINDEX | sed -n 2p | sed 's/^V://')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+username=$1
+token=$2
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/qbittorrent/qbittorrent" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
