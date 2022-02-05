db08220ff5841dca74b63be180a9ab130364584c
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:00:27 +0100

    Update jdownloader-description.sh

--- .templates/jdownloader-description.sh
+++ .templates/jdownloader-description.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/alpine/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+## NO github Repo ##
+## set own DESCRIPTION ##
+DESCRIPTION="Docker container for JDownloader 2"
+printf "%s" "${DESCRIPTION}"
