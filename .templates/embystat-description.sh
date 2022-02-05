6e37a6572fe98b40f3e8d73e6d33197c1f663175
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 16:37:48 +0100

    Update embystat-description.sh

--- .templates/embystat-description.sh
+++ .templates/embystat-description.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -u $username:$token -sX GET "https://api.github.com/repos/mregni/EmbyStat/releases" | jq -r 'first(.[] | .tag_name)')"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/mregni/EmbyStat" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
 
