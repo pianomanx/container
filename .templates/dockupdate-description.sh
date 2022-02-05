c174377319d3875c0632ada14188583cb44f760d
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 17:33:21 +0100

    Update dockupdate-description.sh

--- .templates/dockupdate-description.sh
+++ .templates/dockupdate-description.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+username=$1
+token=$2
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/dockupdater/dockupdater" | jq -r '.description')"
+printf "%s" "${DESCRIPTION}"
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/alpine/tags" | jq --raw-output '.[] | select(.name | contains(".")) | .name' | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
