5def09144e392a5ba445a02226705431c9d50be6
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:07:39 +0100

    Update alpine-description.sh

--- .templates/alpine-description.sh
+++ .templates/alpine-description.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 username=$1
 token=$2
-DESCRIPTION="$(curl -sX GET "https://api.github.com/repos/alpinelinux/docker-alpine" | jq -r '.description')"
+DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/zSeriesGuy/Tautulli" | jq -r '.description')"
 printf "%s" "${DESCRIPTION}"
