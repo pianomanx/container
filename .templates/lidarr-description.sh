00e34fbac55721abfe1ee458acf725415e14f660
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:05:11 +0100

    Update lidarr-description.sh

--- .templates/lidarr-description.sh
+++ .templates/lidarr-description.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 username=$1
 token=$2
-
 DESCRIPTION="$(curl -u $username:$token -sX GET "https://api.github.com/repos/lidarr/lidarr" | jq -r '.description')"
 printf "%s" "${DESCRIPTION}"
