16480a37f46b2d6cfca177e2eac84c2c8f60c8bb
doob187 <60312740+doob187@users.noreply.github.com>
2022-02-05 18:22:57 +0100

    Update ubuntu-description.sh

--- .templates/ubuntu-description.sh
+++ .templates/ubuntu-description.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/ubuntu/tags" | jq --raw-output '.[] | select(.name | contains("focal")) | .name'  | tail -n1)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+## NO github Repo ##
+## set own DESCRIPTION ##
+DESCRIPTION="Dockerbase Image with Ubuntu"
+printf "%s" "${DESCRIPTION}"
